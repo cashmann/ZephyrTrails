09f1e744b653370d6caff2cb0a362fe7e7d4ee73
Zeraphyr <69654904+Zeraphyr@users.noreply.github.com>
2026-09-19 12:12:31 -0500

    add bootstrap

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module ZephyrTrails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.dartsass.build_options << "--quiet-deps"
   end
 end
